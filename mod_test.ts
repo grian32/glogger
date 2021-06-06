dbdac2cb4365aacbf5553a14c01abe847e9c5f5f
Grian <grianguy32@gmail.com>
2021-06-06 12:59:22 +0300

    hopefully fix tests

--- mod_test.ts
+++ mod_test.ts
@@ -1,3 +1,4 @@
+import { assertEquals } from "https://deno.land/std@0.97.0/testing/asserts.ts";
 import { assert } from "./deps.ts";
 
 const decoder = new TextDecoder();
@@ -7,6 +8,9 @@ Deno.test("logger.info()", async () => {
     const p = Deno.run({
         cmd: ["deno", "run", "./testfiles/info.ts"],
         stdout: "piped",
+        env: {
+            "NO_COLOR": "",
+        }
     });
     const stdout = decoder.decode(await p.output());
     p.close();
@@ -17,6 +21,9 @@ Deno.test("logger.error()", async () => {
     const p = Deno.run({
         cmd: ["deno", "run", "./testfiles/error.ts"],
         stdout: "piped",
+        env: {
+            "NO_COLOR": "",
+        }
     });
     const stdout = decoder.decode(await p.output());
     p.close();
@@ -27,6 +34,9 @@ Deno.test("logger.debug()", async () => {
     const p = Deno.run({
         cmd: ["deno", "run", "./testfiles/debug.ts"],
         stdout: "piped",
+        env: {
+            "NO_COLOR": "",
+        }
     });
     const stdout = decoder.decode(await p.output());
     p.close();
@@ -37,7 +47,9 @@ Deno.test("logger.warn()", async () => {
     const p = Deno.run({
         cmd: ["deno", "run", "./testfiles/warn.ts"],
         stdout: "piped",
-        stderr: "piped",
+        env: {
+            "NO_COLOR": "",
+        }
     });
     const stdout = decoder.decode(await p.output());
     p.close();
