08f125b7f3ebf423d42175d4c684e4007a8ce41c
Grian <grianguy32@gmail.com>
2021-06-06 12:18:49 +0300

    finalize tests

--- mod_test.ts
+++ mod_test.ts
@@ -1,19 +1,56 @@
-// TODO: change to deps.ts
-import { brightBlue, brightCyan, cyan, green, yellow, red } from "https://deno.land/std@0.97.0/fmt/colors.ts";
-import { assert, assertEquals } from "https://deno.land/std@0.97.0/testing/asserts.ts";
+import { assert } from "./deps.ts";
 
 const decoder = new TextDecoder();
 
+// sets NO_COLOR so there's no color codes polluting the output.
 Deno.test("logger.info()", async () => {
     const p = Deno.run({
         cmd: ["deno", "run", "testfiles/info.ts"],
         stdout: "piped",
         env: {
-            "NO_COLOR": "", // sets NO_COLOR so there's no color codes polluting the output.
+            "NO_COLOR": "",
         }
     });
     const stdout = decoder.decode(await p.output());
     p.close();
-    assertEquals("dn", stdout)
-    // TODO: regex
+    assert(/\[\d{4}\/\d{2}\/\d{2} \d{2}:\d{2}:\d{2} (?:AM|PM)\] \[(?:(?:.+\.ts|.+\.js)|(?:.+\.ts@.+|.+\.js@.+))\] \[INFO\] .+\n/gm.test(stdout));
+})
+
+Deno.test("logger.error()", async () => {
+    const p = Deno.run({
+        cmd: ["deno", "run", "testfiles/error.ts"],
+        stdout: "piped",
+        env: {
+            "NO_COLOR": "",
+        }
+    });
+    const stdout = decoder.decode(await p.output());
+    p.close();
+    assert(/\[\d{4}\/\d{2}\/\d{2} \d{2}:\d{2}:\d{2} (?:AM|PM)\] \[(?:(?:.+\.ts|.+\.js)|(?:.+\.ts@.+|.+\.js@.+))\] \[ERROR\] .+\n/gm.test(stdout));
+})
+
+Deno.test("logger.debug()", async () => {
+    const p = Deno.run({
+        cmd: ["deno", "run", "testfiles/debug.ts"],
+        stdout: "piped",
+        env: {
+            "NO_COLOR": "",
+        }
+    });
+    const stdout = decoder.decode(await p.output());
+    p.close();
+    assert(/\[\d{4}\/\d{2}\/\d{2} \d{2}:\d{2}:\d{2} (?:AM|PM)\] \[(?:(?:.+\.ts|.+\.js)|(?:.+\.ts@.+|.+\.js@.+))\] \[DEBUG\] .+\n/gm.test(stdout));
+})
+
+Deno.test("logger.warn()", async () => {
+    const p = Deno.run({
+        cmd: ["deno", "run", "testfiles/warn.ts"],
+        stdout: "piped",
+        env: {
+            "NO_COLOR": "",
+        }
+    });
+    const stdout = decoder.decode(await p.output());
+    p.close();
+    assert(/\[\d{4}\/\d{2}\/\d{2} \d{2}:\d{2}:\d{2} (?:AM|PM)\] \[(?:(?:.+\.ts|.+\.js)|(?:.+\.ts@.+|.+\.js@.+))\] \[WARN\] .+\n/gm.test(stdout));
 })
