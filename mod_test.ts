8e8a040d5034ed9c67f7e1988b82ab78f903a1b2
grian <54550982+grian32@users.noreply.github.com>
2021-06-06 12:33:29 +0300

    Update mod_test.ts

--- mod_test.ts
+++ mod_test.ts
@@ -5,11 +5,8 @@ const decoder = new TextDecoder();
 // sets NO_COLOR so there's no color codes polluting the output.
 Deno.test("logger.info()", async () => {
     const p = Deno.run({
-        cmd: ["deno", "run", "testfiles/info.ts"],
+        cmd: ["deno", "run", "./testfiles/info.ts"],
         stdout: "piped",
-        env: {
-            "NO_COLOR": "",
-        }
     });
     const stdout = decoder.decode(await p.output());
     p.close();
@@ -19,11 +16,8 @@ Deno.test("logger.info()", async () => {
 
 Deno.test("logger.error()", async () => {
     const p = Deno.run({
-        cmd: ["deno", "run", "testfiles/error.ts"],
+        cmd: ["deno", "run", "./testfiles/error.ts"],
         stdout: "piped",
-        env: {
-            "NO_COLOR": "",
-        }
     });
     const stdout = decoder.decode(await p.output());
     p.close();
@@ -33,11 +27,8 @@ Deno.test("logger.error()", async () => {
 
 Deno.test("logger.debug()", async () => {
     const p = Deno.run({
-        cmd: ["deno", "run", "testfiles/debug.ts"],
+        cmd: ["deno", "run", "./testfiles/debug.ts"],
         stdout: "piped",
-        env: {
-            "NO_COLOR": "",
-        }
     });
     const stdout = decoder.decode(await p.output());
     p.close();
@@ -47,11 +38,8 @@ Deno.test("logger.debug()", async () => {
 
 Deno.test("logger.warn()", async () => {
     const p = Deno.run({
-        cmd: ["deno", "run", "testfiles/warn.ts"],
+        cmd: ["deno", "run", "./testfiles/warn.ts"],
         stdout: "piped",
-        env: {
-            "NO_COLOR": "",
-        }
     });
     const stdout = decoder.decode(await p.output());
     p.close();
