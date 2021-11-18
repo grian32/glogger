3394530cced543be9321fd0769f62efbe776caea
grian32 <grian32@users.noreply.github.com>
2021-11-18 00:44:25 +0000

    chore: Update dependencies

--- deps.ts
+++ deps.ts
@@ -5,16 +5,16 @@ export {
     green,
     yellow,
     red,
-} from "https://deno.land/std@0.113.0/fmt/colors.ts";
+} from "https://deno.land/std@0.115.1/fmt/colors.ts";
 
 export {
     format
-} from "https://deno.land/std@0.113.0/datetime/mod.ts";
+} from "https://deno.land/std@0.115.1/datetime/mod.ts";
 
 export { 
     assert
-} from "https://deno.land/std@0.113.0/testing/asserts.ts";
+} from "https://deno.land/std@0.115.1/testing/asserts.ts";
 
 export {
     ensureDirSync,
-} from "https://deno.land/std@0.113.0/fs/mod.ts";
+} from "https://deno.land/std@0.115.1/fs/mod.ts";
